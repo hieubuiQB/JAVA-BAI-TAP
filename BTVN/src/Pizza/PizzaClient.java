67b1a81370ebfc082781dc3ffaa7568d9e87e1f6
Hiếu Bùi <97832786+hieubuiQB@users.noreply.github.com>
2022-10-12 19:27:16 +0700

    Add files via upload

---
+++ BTVN/src/Pizza/PizzaClient.java
@@ -0,0 +1,41 @@
+package Pizza;
+
+public class PizzaClient {
+
+
+
+
+    public void run() {
+        Pizza pz1 = new Pizza(6,new Circle(5));
+        System.out.println(pz1);
+        Pizza pz2 = new Pizza(7,new Circle(4));
+        System.out.println(pz2);
+        Pizza pz3 = new Pizza(8,new Circle(3));
+        System.out.println(pz3);
+        Pizza pz4 = new Pizza(9,new Circle(2));
+        System.out.println(pz4);
+
+
+
+        PizzaDeal pd = new PizzaDeal();
+        System.out.println(pz1 + " có ưu đãi tốt hơn " + pz2
+
+                + ": " + pd.betterDeal(pz1, pz2));
+
+
+
+
+
+
+    }
+
+
+    public static void main (String[] nu) {
+
+        new PizzaClient().run();
+
+    }
+
+
+
+}
