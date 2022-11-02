a9a9ed72b9e63df2c20401785631397cf94dbfa2
Hiếu Bùi <97832786+hieubuiQB@users.noreply.github.com>
2022-11-02 22:19:51 +0700

    Add files via upload

---
+++ BTVN/src/Test.java
@@ -0,0 +1,31 @@
+import com.mysql.cj.xdevapi.Result;
+
+import java.sql.*;
+
+public class Test {
+    public static void main(String[] args) {
+        var url = "jdbc:mysql://localhost:3306/ban_sach";
+        var user = "root";
+        var password ="";
+        var sql = "SELECT * FROM hoadon";
+        try (Connection conn = DriverManager.getConnection(url, user, password)) {
+            System.out.println(conn.getCatalog());
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            showInfo(resultSet);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public static void showInfo(ResultSet rs) {
+        try {
+            while (rs.next()) {
+            System.out.println(rs.getInt(1) +"-" + rs.getString(2)
+                    + "-" + rs.getString(3) + "-"+ rs.getFloat(4));
+        }
+        } catch (SQLException throwables) {
+        throwables.printStackTrace();
+        }
+    }
+}
